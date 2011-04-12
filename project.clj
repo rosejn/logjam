1da31857a90ba88033a55db0064a7739651d83af
Jeff Rose <rosejn@gmail.com>
2011-04-12 23:36:52 +0200

    upping clansi version

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A task based logging experiment."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars/clansi "1.2.0-SNAPSHOT"]])
+                 [org.clojars.rosejn/clansi "1.2.0-SNAPSHOT"]])
